c7b75c99c5fcd51b8afb61bfabe5ca861438daa4
jsumita26 <jsumitasumita@gmail.com>
2026-04-14 21:56:33 +0800

    diy-part2.sh

--- scripts/zn-m2/diy-part2.sh
+++ scripts/zn-m2/diy-part2.sh
@@ -1,5 +1,31 @@
 #!/bin/bash
-# minimal build: no extra feeds installation
 set -e
 
-echo "No extra custom feeds to force-install."
+echo "Pruning unused proxy-related feed packages..."
+
+rm -rf feeds/luci/applications/luci-app-brook-server
+rm -rf feeds/luci/applications/luci-app-naiveproxy
+rm -rf feeds/luci/applications/luci-app-ssocks
+rm -rf feeds/luci/applications/luci-app-ssr-libev-server
+rm -rf feeds/luci/applications/luci-app-ssr-plus
+rm -rf feeds/luci/applications/luci-app-trojan-server
+rm -rf feeds/luci/applications/luci-app-v2ray-server
+rm -rf feeds/luci/applications/luci-app-vssr
+
+rm -rf feeds/packages/net/v2raya
+rm -rf feeds/packages/net/brook
+rm -rf feeds/packages/net/naiveproxy
+rm -rf feeds/packages/net/ssocks
+rm -rf feeds/packages/net/trojan
+rm -rf feeds/packages/net/xray-core
+rm -rf feeds/packages/net/sing-box
+rm -rf feeds/packages/net/hysteria
+rm -rf feeds/packages/net/chinadns-ng
+rm -rf feeds/packages/net/dns2socks
+rm -rf feeds/packages/net/dns2tcp
+rm -rf feeds/packages/net/ipt2socks
+rm -rf feeds/packages/net/microsocks
+rm -rf feeds/packages/net/simple-obfs
+rm -rf feeds/packages/net/tcping
+
+echo "Feed prune done."
